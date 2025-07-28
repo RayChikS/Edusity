625eea6d488353664b19b10af62f0521a6708676
RayChik <vatoresp@gmail.com>
2025-07-28 18:10:09 +0300

    header-base-added
    - header base structure and styles
    - hero folder and files

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import css from "./Hero.module.css";
+
+export const Hero = () => {
+  return <div></div>;
+};
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,23 @@
+import React from "react";
 import css from "./Navbar.module.css";
 import logo from "../../assets/logo.png";
 
 export const Navbar = () => {
   return (
-    <nav>
-      <img src={logo} alt="logo image" />
-      <ul>
-        <li>Home</li>
-        <li>Program</li>
-        <li>About us</li>
-        <li>Campus</li>
-        <li>Testimonials</li>
-        <li>Contact us</li>
-      </ul>
-    </nav>
+    <header>
+      <nav className="container">
+        <img className={css.logo} src={logo} alt="logo image" />
+        <ul className={css.navList}>
+          <li className={css.navListItem}>Home</li>
+          <li className={css.navListItem}>Program</li>
+          <li className={css.navListItem}>About us</li>
+          <li className={css.navListItem}>Campus</li>
+          <li className={css.navListItem}>Testimonials</li>
+          <li className={css.navListItem}>
+            <button className={css.btn}>Contact us</button>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
