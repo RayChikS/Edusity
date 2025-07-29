3f2eb1a14f0a7965e35d6d387c4c52118fd3cf27
RayChik <vatoresp@gmail.com>
2025-07-29 21:32:06 +0300

    programs-section
    - created basic structure for programs section
    - basic styles for it
    - added simple animation for cards

--- src/components/Programs/Programs.jsx
+++ src/components/Programs/Programs.jsx
@@ -3,19 +3,34 @@ import css from "./Programs.module.css";
 import program1 from "../../assets/program-1.png";
 import program2 from "../../assets/program-2.png";
 import program3 from "../../assets/program-3.png";
+import programIcon1 from "../../assets/program-icon-1.png";
+import programIcon2 from "../../assets/program-icon-2.png";
+import programIcon3 from "../../assets/program-icon-3.png";
 
 export const Programs = () => {
   return (
     <div className={css.programs}>
-      <ul>
-        <li className={css.program}>
-          <img src="" alt="" />
+      <ul className={css.programsList}>
+        <li className={css.programListItem}>
+          <img className={css.programItemImg} src={program1} alt="card" />
+          <div className={css.caption}>
+            <img className={css.captionIcon} src={programIcon1} alt="icon" />
+            <p className={css.captionText}>Graduation Degree</p>
+          </div>
         </li>
-        <li className={css.program}>
-          <img src="" alt="" />
+        <li className={css.programListItem}>
+          <img className={css.programItemImg} src={program2} alt="card" />
+          <div className={css.caption}>
+            <img className={css.captionIcon} src={programIcon2} alt="icon" />
+            <p className={css.captionText}>Masters Degree</p>
+          </div>
         </li>
-        <li className={css.program}>
-          <img src="" alt="" />
+        <li className={css.programListItem}>
+          <img className={css.programItemImg} src={program3} alt="card" />
+          <div className={css.caption}>
+            <img className={css.captionIcon} src={programIcon3} alt="icon" />
+            <p className={css.captionText}>Post Graduation</p>
+          </div>
         </li>
       </ul>
     </div>
