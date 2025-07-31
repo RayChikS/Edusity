87883f1eebb1b13a73b3401e22c4f86a3caf736f
RayChik <vatoresp@gmail.com>
2025-07-31 17:01:11 +0300

    header-background-change
    - added functional for chanching header background while scroled for 50 px

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import css from "./Navbar.module.css";
 import logo from "../../assets/logo.png";
 
 export const Navbar = () => {
+  const [sticky, setSticky] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      window.scrollY > 50 ? setSticky(true) : setSticky(false);
+    });
+  }, []);
+
   return (
-    <nav className="container">
+    <nav className={`container ${sticky ? css.darkBar : ""}`}>
       <img className={css.logo} src={logo} alt="logo image" />
       <ul className={css.navList}>
         <li className={css.navListItem}>Home</li>
